cc86353d838054d865e69603870c66367b88ebca
jarol1590 <jarol1590@gmail.com>
2024-12-07 15:02:30 -0500

    creación modulo y componente login

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  /*{
-    path: '',
-    loadChildren: () => import('../app/modules/grafo/grafo.module').then(m => m.GrafoModule)
-  }*/
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
 ];
 
 @NgModule({
