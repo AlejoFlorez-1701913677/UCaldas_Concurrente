03a7b024fef93389f8d6ac4c48e40ef848c32537
jarol1590 <jarol1590@gmail.com>
2024-12-09 20:04:13 -0500

    se crea un guard para que la ruta predeterminada sea LOGIN y se implementa

--- src/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+    canActivate: [AuthGuard]
+   },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige la ruta raíz a 'login'
+  { path: 'login', component: LoginComponent }, // Ruta al componente Login
+  // Agrega otras rutas aquí
 ];
 
 @NgModule({
