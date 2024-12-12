d9a59036133cce554b4ad4474f0a9dda58b5112a
jarol1590 <jarol1590@gmail.com>
2024-12-12 18:14:01 -0500

    registro organizado con el bakend

--- src/frontend/src/app/registro/registro.component.ts
+++ src/frontend/src/app/registro/registro.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ConfigServiceService } from '../services/config-service.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,9 +13,9 @@ export class RegistroComponent {
   registerForm: FormGroup;
   errorMessage: string | null = null;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private http: HttpClient, private configService: ConfigServiceService) {
     this.registerForm = this.fb.group({
-      name: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
@@ -21,11 +23,25 @@ export class RegistroComponent {
 
   onSubmit(): void {
     if (this.registerForm.valid) {
+      const { username, email, password } = this.registerForm.value;
       console.log('Formulario válido, datos:', this.registerForm.value);
       // Procesar datos del formulario aquí
-    } else {
-      this.errorMessage = 'Por favor, complete todos los campos correctamente.';
-    }
+      this.http.post(`${this.configService.apiUrl}/User/register`, { username, email, password })
+      .subscribe({
+        next: (response: any) => {
+          // Suponiendo que el backend devuelve un token en la respuesta
+          if (response && response.access_token) {
+            this.router.navigate(['/login']); // Redirige al login
+          }
+        },
+        error: (error) => {
+          console.error('Error de registro:', error);
+          this.errorMessage = 'Correo o contraseña incorrectos'; // Manejo de error
+        }
+      });
+  } else {
+    this.errorMessage = 'Por favor completa todos los campos correctamente';
+  }
   }
 
   navigateToLogin(): void {
