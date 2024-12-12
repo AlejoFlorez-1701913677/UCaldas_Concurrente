35d55d467c0fb541609135b78b29a87f8433aa35
Junior <jecheverri@consensussa.com>
2024-12-12 14:17:46 -0500

    Login y generación token

--- src/backend/routes/user_route.py
+++ src/backend/routes/user_route.py
@@ -1,24 +1,28 @@
 from fastapi import APIRouter, HTTPException, Depends
 from motor.motor_asyncio import AsyncIOMotorDatabase
-from passlib.context import CryptContext
 from database.mongo import Database
 
 from models.user_create import UserCreate
+from models.login_data import LoginData
 from response.user_response import UserResponse
 from .publisher import send_registration_email
 import secrets
 import jwt
 from datetime import datetime, timedelta
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import os
+from argon2 import PasswordHasher
 
 routerUser = APIRouter()
-
-# Configuración para hash de contraseñas
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ph = PasswordHasher()
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return ph.hash(password)
+
+def verify_password(password: str, hash: str) -> bool:
+    try:
+        return ph.verify(hash, password)
+    except:
+        return False
 
 def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
@@ -28,55 +32,44 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 @routerUser.post("/register", response_model=UserResponse, status_code=201)
-async def register_user(
-    user: UserCreate,
-    db: AsyncIOMotorDatabase = Depends(Database.get_db)
-):
+async def register_user(user: UserCreate, db: AsyncIOMotorDatabase = Depends(Database.get_db)):
     users_collection = db["users"]
     existing_user = await users_collection.find_one({"email": user.email})
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email ya registrado")
+        raise HTTPException(status_code=400, detail="Email already registered")
 
     hashed_password = get_password_hash(user.password)
-    # Generar una llave de acceso segura
     access_key = secrets.token_urlsafe(16)
 
     new_user = {
         "username": user.username,
         "email": user.email,
         "password": hashed_password,
-        "access_key": access_key  # Almacenar la llave de acceso en la base de datos
+        "access_key": access_key
     }
     result = await users_collection.insert_one(new_user)
     user_id = str(result.inserted_id)
-
-    # Enviar un mensaje para registrar el correo con la llave de acceso
     send_registration_email(user.email, access_key)
-
     return UserResponse(id=user_id, username=user.username, email=user.email)
 
 @routerUser.post("/login")
-async def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    db: AsyncIOMotorDatabase = Depends(Database.get_db)
-):
-    print(form_data)
+async def login(login_data: LoginData, db: AsyncIOMotorDatabase = Depends(Database.get_db)):
     user_collection = db["users"]
-    user = await user_collection.find_one({"username": form_data.username})
+    user = await user_collection.find_one({"username": login_data.username})
+    #print("Se encontró un usuario" + user)
+    print(user)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    
-    # Verificar contraseña y access_key
-    password_correct = pwd_context.verify(form_data.password, user["password"])
-    access_key_correct = form_data.username + "your_salt" == user["access_key"]  # Simulando una comparación
-    if not password_correct or not access_key_correct:
-        raise HTTPException(status_code=400, detail="Invalid authentication credentials")
 
-    # Crear token JWT
-    access_token_expires = timedelta(minutes=int(os.getenv("TOKEN_MINUTES")))
-    access_token = create_access_token(
-        data={"sub": user["username"]}, expires_delta=access_token_expires
-    )
+    password_correct = verify_password(login_data.password, user["password"])
+    #print("Contraseña correcta" + password_correct)
+    print(password_correct)
+    #access_key_correct = login_data.access_key == user["access_key"]
+    #if not password_correct or not access_key_correct:
+        #raise HTTPException(status_code=400, detail="Invalid authentication credentials")
+
+    access_token_expires = timedelta(minutes=int(os.getenv("TOKEN_MINUTES", "30")))
+    access_token = create_access_token(data={"sub": user["username"]}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 @routerUser.get("/createCollection")
